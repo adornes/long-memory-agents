894730df71363f5a67ff33b41300b630b24cca43
Daniel Adornes <daniel.adornes@gmail.com>
2025-04-20 16:18:08 -0300

    Isolating db operations before moving them to the API

--- client/client_with_pgvector.py
+++ client/client_with_pgvector.py
@@ -19,15 +19,15 @@ parser = argparse.ArgumentParser(description="Choose similarity search type.")
 parser.add_argument(
     "--similarity-search-threshold",
     default=0.75,
-    required=False,    
-    help="Threshold of cosine similarity to return from similarity search.",
+    required=False,
+    help="Threshold of cosine similarity to return from similarity search."
 )
 
 parser.add_argument(
     "--similarity-search-limit",
     default=5,
-    required=False,    
-    help="Limit of results to return from similarity search.",
+    required=False,
+    help="Limit of results to return from similarity search."
 )
 
 args = parser.parse_args()
@@ -46,13 +46,12 @@ embeddings = OpenAIEmbeddings()
 
 
 # Define an async function to process chunks from the agent
-async def process_chunks(chunk, conn):
+async def process_chunks(chunk):
     """
     Asynchronously processes a chunk from the agent and displays information about tool calls or the agent's answer.
 
     Parameters:
         chunk (dict): A dictionary containing information about the agent's messages.
-        conn (asyncpg.Connection): The database connection object.
 
     Returns:
         None
@@ -87,7 +86,7 @@ async def process_chunks(chunk, conn):
                     # Display an informative message with tool call details
                     rich.print(
                         f"\nThe agent is calling the tool [on deep_sky_blue1]{tool_name}[/on deep_sky_blue1] with the query [on deep_sky_blue1]{tool_query}[/on deep_sky_blue1]. Please wait for the agent's answer[deep_sky_blue1]...[/deep_sky_blue1]",
-                        style="deep_sky_blue1",
+                        style="deep_sky_blue1"
                     )
             else:
                 # If the message doesn't contain tool calls, extract and display the agent's answer
@@ -102,17 +101,11 @@ async def process_chunks(chunk, conn):
                 agent_answer_embedding = embeddings_response
 
                 # Insert the agent's answer and its embedding vector into the database
-                await conn.execute(
-                    "INSERT INTO chat (role, message, embedding_vector) VALUES (%s, %s, %s)",
-                    (
-                        "agent",
-                        agent_answer,
-                        agent_answer_embedding,
-                    ),
-                )
+                await persist_message("agent", agent_answer, agent_answer_embedding)
 
                 # Display the agent's answer
                 rich.print(f"\nAgent:\n{agent_answer}", style="black on white")
+
 
 def connection_pool():
     return AsyncConnectionPool(
@@ -122,9 +115,10 @@ def connection_pool():
         kwargs={
             "autocommit": True,
             "prepare_threshold": 0,
-            "row_factory": dict_row,
-        },
+            "row_factory": dict_row
+        }
     )
+
 
 async def persist_message(role, text, embeddings):
     """
@@ -144,9 +138,54 @@ async def persist_message(role, text, embeddings):
             (
                 role,
                 text,
-                embeddings,
-            ),
+                embeddings
+            )
         )
+
+
+async def similarity_search(message_embedding, similarity_search_threshold, similarity_search_limit):
+    """
+    Performs a similarity search in the database to find the most similar past messages.
+
+    Parameters:
+        message_embedding (vector): The embedding vector of the message.
+        similarity_search_threshold (float): The threshold of cosine similarity to return from similarity search.
+        similarity_search_limit (int): The limit of results to return from similarity search.
+
+    Returns:
+        list: A list of tuples containing the message and its cosine similarity.
+    """
+    async with connection_pool() as pool, pool.connection() as conn:
+        similarity_search = await conn.execute(
+            """
+            WITH ranked_messages AS (
+                SELECT 
+                    message,
+                    1 - (embedding_vector <=> %s::vector) AS cosine_similarity,
+                    ROW_NUMBER() OVER (
+                        PARTITION BY message 
+                        ORDER BY 1 - (embedding_vector <=> %s::vector) DESC
+                    ) as rn
+                FROM chat
+                WHERE 1 - (embedding_vector <=> %s::vector) >= %s
+            )
+            SELECT message, cosine_similarity
+            FROM ranked_messages 
+            WHERE rn = 1 
+            ORDER BY cosine_similarity DESC
+            LIMIT %s;
+            """,
+            (
+                message_embedding,
+                message_embedding,
+                message_embedding,
+                similarity_search_threshold,
+                similarity_search_limit
+            )
+        )
+
+        return await similarity_search.fetchall()
+
 
 # Define an async function to chat with the agent
 async def main():
@@ -175,147 +214,115 @@ async def main():
     - "threshold": Returns all past messages that have a cosine similarity equal to or greater than 0.75 to the latest user message.
     """
 
-    # Connect to the PostgreSQL database using an async connection pool
-    async with connection_pool() as pool, pool.connection() as conn:
+    # Create a LangGraph agent
+    langgraph_agent = create_react_agent(model=llm, tools=[])  # Tavily removed
 
-        # Create a LangGraph agent
-        langgraph_agent = create_react_agent(model=llm, tools=[]) # Tavily removed
+    # Loop until the user chooses to quit the chat
+    while True:
+        # Get the user's question and display it in the terminal
+        user_question = input("\nUser:\n")
 
-        # Loop until the user chooses to quit the chat
-        while True:
-            # Get the user's question and display it in the terminal
-            user_question = input("\nUser:\n")
-
-            # Check if the user wants to quit the chat
-            if user_question.lower() == "quit":
-                rich.print(
-                    "\nAgent:\nHave a nice day! :wave:\n", style="black on white"
-                )
-                break
-
-            # Create the embedding vector for the user's question
-            embeddings_response = embeddings.embed_query(user_question)
-
-            # Retrieve the embedding vector for the user's question from the Embed LLM response
-            user_question_embedding = embeddings_response
-
-            # Insert the user's question and its embedding vector into the database
-            await persist_message("user", user_question, user_question_embedding)
-
-        
-            # Return the top 5 most similar past messages (i.e., user questions or LangGraph agent answers) from PostgreSQL with the highest cosine similarity
-            # Cosine similarity is calculated against the embedding vector of the latest user message
-            # Duplicates are removed by selecting only the first occurrence of each message
-            # Results are ordered from most to least similar
-            similarity_search = await conn.execute(
-                """
-                WITH ranked_messages AS (
-                    SELECT 
-                        message,
-                        1 - (embedding_vector <=> %s::vector) AS cosine_similarity,
-                        ROW_NUMBER() OVER (
-                            PARTITION BY message 
-                            ORDER BY 1 - (embedding_vector <=> %s::vector) DESC
-                        ) as rn
-                    FROM chat
-                    WHERE 1 - (embedding_vector <=> %s::vector) >= %s
-                )
-                SELECT message, cosine_similarity
-                FROM ranked_messages 
-                WHERE rn = 1 
-                ORDER BY cosine_similarity DESC
-                LIMIT %s;
-                """,
-                (
-                    user_question_embedding,
-                    user_question_embedding,
-                    user_question_embedding,
-                    args.similarity_search_threshold,
-                    args.similarity_search_limit
-                ),
-            )
-
-            # Fetch the similarity search results
-            similarity_search_results = await similarity_search.fetchall()
-
+        # Check if the user wants to quit the chat
+        if user_question.lower() == "quit":
             rich.print(
-                "\n============================================================\n"
+                "\nAgent:\nHave a nice day! :wave:\n", style="black on white"
             )
+            break
 
-            # Display all similarity search result messages
-            # Those will be passed to the LangGraph agent as the system message
+        # Create the embedding vector for the user's question
+        embeddings_response = embeddings.embed_query(user_question)
+
+        # Retrieve the embedding vector for the user's question from the Embed LLM response
+        user_question_embedding = embeddings_response
+
+        # Insert the user's question and its embedding vector into the database
+        await persist_message("user", user_question, user_question_embedding)
+
+        # Fetch the similarity search results
+        similarity_search_results = await similarity_search(
+            user_question_embedding,
+            args.similarity_search_threshold,
+            args.similarity_search_limit
+        )
+
+        rich.print(
+            "\n============================================================\n"
+        )
+
+        # Display all similarity search result messages
+        # Those will be passed to the LangGraph agent as the system message
+        rich.print(
+            "[on deep_sky_blue1]Similarity search results:[/on deep_sky_blue1]",
+            style="deep_sky_blue1"
+        )
+
+        rich.print(
+            f"Here are the top {args.similarity_search_limit} most similar past messages with a cosine similarity equal to or greater than {args.similarity_search_threshold} to the latest user's question:",
+            style="deep_sky_blue1"
+        )
+
+        for i, query_result in enumerate(similarity_search_results):
             rich.print(
-                "[on deep_sky_blue1]Similarity search results:[/on deep_sky_blue1]",
-                style="deep_sky_blue1",
+                f"Message #{i+1} (cosine similarity = {query_result['cosine_similarity']:.2f}): {query_result['message']}",
+                style="deep_sky_blue1"
             )
 
+        # Prepare messages (i.e., human and system messages) to be passed to the LangGraph agent
+        # Add the user's question to the HumanMessage object
+        messages = [HumanMessage(content=user_question)]
+
+        # Create a list to store the messages
+        similar_messages = []
+
+        # Iterate over the similarity search results and add them to the list
+        for query_result in similarity_search_results:
+            similar_messages.append(query_result["message"])
+
+        # If there are similar messages returned from the similarity search, add them to the SystemMessage object
+        if len(similar_messages) > 0:
+            join_similar_messages = "\n".join(
+                [f"- {message}" for message in similar_messages]
+            )
+            system_message = f"To answer the user's question, use this information which is part of the past conversation as a context:\n{join_similar_messages}"
+            messages.insert(0, SystemMessage(content=system_message))
+
+        # Initialize the system message and human message
+        system_message = None
+        human_message = None
+
+        # Iterate over the messages and extract the system and human messages
+        for message in messages:
+            # Check if the message is a system message
+            if isinstance(message, SystemMessage):
+                system_message = message
+
+            # Check if the message is a human message
+            elif isinstance(message, HumanMessage):
+                human_message = message
+
+        # Display all messages that will be passed to the LangGraph agent (system and human messages)
+        rich.print(
+            "[on deep_sky_blue1]\nMessages passed to the LangGraph agent:[/on deep_sky_blue1]",
+            style="deep_sky_blue1"
+        )
+
+        if system_message:
             rich.print(
-                f"Here are the top {args.similarity_search_limit} most similar past messages with a cosine similarity equal to or greater than {args.similarity_search_threshold} to the latest user's question:",
-                style="deep_sky_blue1",
+                f"The system message:\n-----------------------\n{system_message.content}",
+                style="deep_sky_blue1"
             )
-
-            for i, query_result in enumerate(similarity_search_results):
-                rich.print(
-                    f"Message #{i+1} (cosine similarity = {query_result['cosine_similarity']:.2f}): {query_result['message']}",
-                    style="deep_sky_blue1",
-                )
-
-            # Prepare messages (i.e., human and system messages) to be passed to the LangGraph agent
-            # Add the user's question to the HumanMessage object
-            messages = [HumanMessage(content=user_question)]
-
-            # Create a list to store the messages
-            similar_messages = []
-
-            # Iterate over the similarity search results and add them to the list
-            for query_result in similarity_search_results:
-                similar_messages.append(query_result["message"])
-
-            # If there are similar messages returned from the similarity search, add them to the SystemMessage object
-            if len(similar_messages) > 0:
-                join_similar_messages = "\n".join(
-                    [f"- {message}" for message in similar_messages]
-                )
-                system_message = f"To answer the user's question, use this information which is part of the past conversation as a context:\n{join_similar_messages}"
-                messages.insert(0, SystemMessage(content=system_message))
-
-            # Initialize the system message and human message
-            system_message = None
-            human_message = None
-
-            # Iterate over the messages and extract the system and human messages
-            for message in messages:
-                # Check if the message is a system message
-                if isinstance(message, SystemMessage):
-                    system_message = message
-
-                # Check if the message is a human message
-                elif isinstance(message, HumanMessage):
-                    human_message = message
-
-            # Display all messages that will be passed to the LangGraph agent (system and human messages)
+        if human_message:
             rich.print(
-                "[on deep_sky_blue1]\nMessages passed to the LangGraph agent:[/on deep_sky_blue1]",
-                style="deep_sky_blue1",
+                f"\nThe human message:\n-----------------------\n{human_message.content}",
+                style="deep_sky_blue1"
             )
 
-            if system_message:
-                rich.print(
-                    f"The system message:\n-----------------------\n{system_message.content}",
-                    style="deep_sky_blue1",
-                )
-            if human_message:
-                rich.print(
-                    f"\nThe human message:\n-----------------------\n{human_message.content}",
-                    style="deep_sky_blue1",
-                )
+        rich.print("\n============================================================")
 
-            rich.print("\n============================================================")
-
-            # Use the async stream method of the LangGraph agent to get the agent's answer
-            async for chunk in langgraph_agent.astream({"messages": messages}):
-                # Process the chunks from the agent
-                await process_chunks(chunk, conn)
+        # Use the async stream method of the LangGraph agent to get the agent's answer
+        async for chunk in langgraph_agent.astream({"messages": messages}):
+            # Process the chunks from the agent
+            await process_chunks(chunk)
 
 
 if __name__ == "__main__":
